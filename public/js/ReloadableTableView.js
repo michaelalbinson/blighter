72f553ec1e83768ed3b57b30c324aaea78282ca1
michaelalbinson <mikealbinson@gmail.com>
2023-08-18 17:42:29 -0400

    Generify client list to make code more manageable

---
+++ public/js/ReloadableTableView.js
@@ -0,0 +1,122 @@
+'use strict';
+
+class ReloadableListView {
+    constructor(listRootID, pageSize, getItemsFn, getListCellFn, filterFn) {
+        this.listRoot = document.getElementById(listRootID);
+        this._renderSearchInput();
+        this._renderListTarget();
+        this._renderPaginationButtons();
+
+        this._getItemsFn = getItemsFn;
+        this._getListCellFn = getListCellFn;
+        this._filterFn = filterFn;
+
+        this._itemList = [];
+        this._filteredItemList = [];
+        this._currentStartIndex = 0;
+        this._currentListElement = null;
+        this._pageSize = pageSize;
+    }
+
+    async _loadList() {
+        this._itemList = await this._getItemsFn();
+        this._filteredItemList = this._itemList;
+    }
+
+    _renderList() {
+        if (this._currentListElement)
+            this.listTarget.removeChild(this._currentListElement);
+
+        let newList = document.createElement('ul');
+        if (this._filteredItemList.length !== 0) {
+            const elsToRender = this._filteredItemList.slice(this._currentStartIndex, this._currentStartIndex + this._pageSize);
+            for (let el of elsToRender) {
+                const li = this._getListCellFn(el);
+                newList.appendChild(li);
+            }
+        } else {
+            newList = document.createElement('span');
+            newList.innerText = 'No matches found';
+        }
+
+        this.listTarget.appendChild(newList);
+        this._currentListElement = newList;
+    }
+
+    _renderListTarget() {
+        this.listTarget = document.createElement('div');
+        this.listRoot.appendChild(this.listTarget);
+    }
+
+    async render() {
+        if (this._filteredItemList.length === 0)
+            await this._loadList();
+
+        this._renderList();
+    }
+
+    _renderPaginationButtons() {
+        const pageRight = button('>', () => {
+            if (this._currentStartIndex < (this._filteredItemList.length - (this._pageSize - 1)))
+                this._currentStartIndex += this._pageSize;
+
+            this._renderList();
+        });
+
+        const pageLeft = button('<', () => {
+            if (this._currentStartIndex !== 0)
+                this._currentStartIndex -= this._pageSize;
+
+            this._renderList();
+        });
+
+        const toStartButton = button('<<', () => {
+            this._currentStartIndex = 0;
+            this._renderList();
+        });
+
+        const toEndButton = button('>>', () => {
+            this._currentStartIndex = this._pageSize * (Math.floor(this._filteredItemList.length / this._pageSize));
+            this._renderList();
+        });
+
+        const lbg = document.createElement('div');
+        lbg.id = 'trailing-button-group';
+        lbg.appendChild(toStartButton);
+        lbg.appendChild(pageLeft);
+        lbg.appendChild(pageRight);
+        lbg.appendChild(toEndButton);
+        this.listRoot.appendChild(lbg);
+    }
+
+    _renderSearchInput() {
+        const searchForm = document.createElement('form');
+        searchForm.id = 'search-form';
+        const label = document.createElement('label');
+        label.for = 'search-input';
+        label.style = 'display: none';
+        label.innerText = 'Search';
+        const input = document.createElement('input');
+        input.type = 'search';
+        input.name = 'search';
+        input.id = 'search-input';
+        input.placeholder = '🔎 Quick filter...';
+        input.addEventListener('keyup', () => {
+            const searchTerm = input.value;
+            if (!searchTerm || searchTerm === '') {
+                this._filteredItemList = this._itemList;
+                return this._renderList();
+            }
+
+            this._currentStartIndex = 0;
+            this._filteredItemList = this._itemList.filter(entry => {
+                return this._filterFn(entry, searchTerm);
+            });
+
+            this._renderList();
+        });
+        searchForm.appendChild(label);
+        searchForm.appendChild(input);
+        this.listRoot.appendChild(searchForm);
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,133 +1,80 @@
 window.onload = async () => {
     'use strict';
 
-    const PAGE_SIZE = window.blighterClientSettings.get('pageSize');
+    const getData = async () => {
+        const urlParams = getUrlParams();
+        let url = '/feed';
+        if (window.location.href.includes('single-feed'))
+            url = `/feed?id=${urlParams.id}`;
+        else if (window.location.href.includes('/saved'))
+            url = '/feed?saved=true';
+        else if (window.location.href.includes('/reading-list'))
+            url = '/reading-list-feed'
 
-    const urlParams = getUrlParams();
-    let url = '/feed';
-    if (window.location.href.includes('single-feed'))
-        url = `/feed?id=${urlParams.id}`;
-    else if (window.location.href.includes('/saved'))
-        url = '/feed?saved=true';
+        const data = await fetch(url);
+        if (!data.ok)
+            throw new Error('Failed to load feed data');
 
-    const data = await fetch(url);
-    if (!data.ok)
-        throw new Error('Failed to load feed data');
-
-    const listing = await data.json();
-    let currentListing = listing;
-    let currentStartIndex = 0;
-    let currentListElement = null;
-    const renderList = () => {
-        currentListElement = generateList(currentListing.slice(currentStartIndex, currentStartIndex + PAGE_SIZE), currentListElement);
-    };
-    renderList();
-
-    const pageRight = button('>', () => {
-        if (currentStartIndex < (currentListing.length - (PAGE_SIZE - 1)))
-            currentStartIndex += PAGE_SIZE;
-
-        renderList();
-    });
-
-    const pageLeft = button('<', () => {
-        if (currentStartIndex !== 0)
-            currentStartIndex -= PAGE_SIZE;
-
-        renderList();
-    });
-
-    const toStartButton = button('<<', () => {
-        currentStartIndex = 0;
-        renderList();
-    });
-
-    const toEndButton = button('>>', () => {
-        currentStartIndex = PAGE_SIZE * (Math.floor(currentListing.length / PAGE_SIZE));
-        renderList();
-    });
-
-    const lbg = document.getElementById('trailing-button-group');
-    lbg.appendChild(toStartButton);
-    lbg.appendChild(pageLeft);
-    lbg.appendChild(pageRight);
-    lbg.appendChild(toEndButton);
-
-    const search = document.getElementById('search-input');
-    search.addEventListener('keyup', () => {
-        const searchTerm = search.value;
-        if (!searchTerm || searchTerm === '') {
-            currentListing = listing;
-            return renderList();
-        }
-
-        currentStartIndex = 0;
-        currentListing = listing.filter(entry => {
-            return (entry.title + ' ' + entry.categories + ' ' + entry.feed.name).toLowerCase().includes(searchTerm.toLowerCase());
-        });
-
-        renderList();
-    });
-
-    function generateList(listing, currentListElement) {
-        const listRoot = document.getElementById('list');
-        if (currentListElement)
-            listRoot.removeChild(currentListElement);
-
-        const newList = document.createElement('ul');
-        for (let el of listing) {
-            const link = document.createElement('a');
-            link.href = el.link;
-            link.target = '_blank';
-            link.innerText = el.title;
-            const span = document.createElement('span');
-            span.innerText = new Date(el.pubDate).toString() + " - ";
-            const a2 = document.createElement('a');
-            a2.innerText = el.feed.name;
-            a2.href = `/single-feed?id=${el.feed.id}`;
-            span.appendChild(a2);
-            const span2 = document.createElement('span');
-            span2.innerText = el.categories;
-            const span3 = document.createElement('span');
-            span3.classList.add('links');
-            const saveButton = button(el.saved ? 'Unsave' : 'Save', async () => {
-                const res = await fetch('/save-item', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ itemId: el.id, saved: el.saved })
-                });
-
-                if (!res.ok) {
-                    console.error('Failed to save item');
-                    return;
-                }
-
-                saveButton.innerText = el.saved ? 'Save' : 'Unsave';
-                if (window.location.href.includes('/saved'))
-                    window.location.reload();
-            });
-            span3.appendChild(saveButton);
-            const span4 = document.createElement('span');
-            span4.innerText = ' - ';
-            span3.appendChild(span4);
-
-            const noteButton = addLink(el.hasNote ? 'View/Edit Note' : 'Add Note', `/item-note?itemId=feed_item-${el.id}`);
-            span3.appendChild(noteButton);
-            const li = document.createElement('li');
-            li.appendChild(link);
-            li.appendChild(document.createElement('br'));
-            li.appendChild(span);
-            li.appendChild(document.createElement('br'));
-            li.appendChild(span2);
-            li.appendChild(document.createElement('br'));
-            li.appendChild(span3);
-            newList.appendChild(li);
-        }
-
-        listRoot.appendChild(newList);
-        return newList;
+        return await data.json();
     }
+
+    const buildListItem = (item) => {
+        const link = document.createElement('a');
+        link.href = item.link;
+        link.target = '_blank';
+        link.innerText = item.title;
+        const span = document.createElement('span');
+        span.innerText = new Date(item.pubDate).toString() + " - ";
+        const a2 = document.createElement('a');
+        a2.innerText = item.feed.name;
+        a2.href = `/single-feed?id=${item.feed.id}`;
+        span.appendChild(a2);
+        const span2 = document.createElement('span');
+        span2.innerText = item.categories;
+        const span3 = document.createElement('span');
+        span3.classList.add('links');
+        const saveButton = button(item.saved ? 'Unsave' : 'Save', async () => {
+            const res = await fetch('/save-item', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ itemId: item.id, saved: item.saved })
+            });
+
+            if (!res.ok) {
+                console.error('Failed to save item');
+                return;
+            }
+
+            saveButton.innerText = item.saved ? 'Save' : 'Unsave';
+            if (window.location.href.includes('/saved'))
+                window.location.reload();
+        });
+        span3.appendChild(saveButton);
+        const span4 = document.createElement('span');
+        span4.innerText = ' - ';
+        span3.appendChild(span4);
+
+        const noteButton = addLink(item.hasNote ? 'View/Edit Note' : 'Add Note', `/item-note?itemId=feed_item-${item.id}`);
+        span3.appendChild(noteButton);
+        const li = document.createElement('li');
+        li.appendChild(link);
+        li.appendChild(document.createElement('br'));
+        li.appendChild(span);
+        li.appendChild(document.createElement('br'));
+        li.appendChild(span2);
+        li.appendChild(document.createElement('br'));
+        li.appendChild(span3);
+        return li;
+    };
+
+    const filterFn = (item, searchTerm) => {
+        return (item.title + ' ' + item.categories + ' ' + item.feed.name).toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    const PAGE_SIZE = window.blighterClientSettings.get('pageSize');
+    const rlv = new ReloadableListView('list', PAGE_SIZE, getData, buildListItem, filterFn);
+    await rlv.render();
 }
