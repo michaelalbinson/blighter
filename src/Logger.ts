ab4d4af2ccb8ce279920a0097208c4d0236cf77c
michaelalbinson <mikealbinson@gmail.com>
2023-08-26 01:46:50 -0400

    Timestamp log messages

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,25 +2,29 @@
 
 export default class Logger {
     static log(msg: string) {
-        console.log(msg);
+        console.log(this._withTimeStamp(msg));
     }
 
     static debug(msg: any) {
         if (!process.env.DEBUG)
             return;
 
-        console.log(msg);
+        console.log(this._withTimeStamp(msg));
     }
 
     static error(msg: string) {
-        console.error(msg);
+        console.error(this._withTimeStamp(msg));
     }
 
     static warn(msg: string) {
-        console.warn(msg);
+        console.warn(this._withTimeStamp(msg));
     }
 
     static info(msg: string) {
-        console.log(msg);
+        console.log(this._withTimeStamp(msg));
+    }
+
+    private static _withTimeStamp(msg: string): string {
+        return `${new Date().toISOString()} ${msg}`;
     }
 }
