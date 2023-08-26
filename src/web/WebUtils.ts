b5a28dc2ebe368c961c94dc754615fbaf3e99d02
michaelalbinson <mikealbinson@gmail.com>
2023-08-26 01:55:42 -0400

    Clean up default error handling

--- src/web/WebUtils.ts
+++ src/web/WebUtils.ts
@@ -4,9 +4,12 @@ import FeedItem from "../db/rss/types/FeedItem";
 import ReadingListItem from "../db/reading_list/types/ReadingListItem";
 import RSSFeedItem from "../db/rss/RSSFeedItem";
 import ReadingListItemDB from "../db/reading_list/ReadingListItemDB";
+import Logger from "../Logger";
+import {Response, Request} from "express";
 
 export default class WebUtils {
     static async resolveItem(itemId: string): Promise<FeedItem | ReadingListItem> {
+        console.log(itemId);
         let item: FeedItem|ReadingListItem;
         if (itemId.includes('feed_item')) {
             const feedItemId = Number(itemId.split('feed_item-')[1])
@@ -19,4 +22,13 @@ export default class WebUtils {
 
         return item;
     };
+
+    static async defaultErrorHandling(req: Request, res: Response, action: () => Promise<void>) {
+        try {
+            await action();
+        } catch (e) {
+            Logger.debug(e);
+            res.status(500).send();
+        }
+    }
 }
--- src/web/feed-item.ts
+++ src/web/feed-item.ts
@@ -5,72 +5,77 @@ import RSSFeedItem from "../db/rss/RSSFeedItem";
 import ReadingListItemDB from "../db/reading_list/ReadingListItemDB";
 import ReadingListItem from "../db/reading_list/types/ReadingListItem";
 import WebUtils from "./WebUtils";
+import Logger from "../Logger";
 
 export default function setupFeedItemRoutes(app: Express) {
     app.post('/save-item', async (req, res) => {
-        try {
+        await WebUtils.defaultErrorHandling(req, res, async () => {
             const itemId = (req.body as {itemId: string}).itemId;
-            const item = await WebUtils.resolveItem(itemId);
-            if (!itemId)
-                return res.status(400).send();
+            if (!itemId) {
+               res.status(400).send();
+               return;
+            }
 
+            const item = await WebUtils.resolveItem(itemId);
             if ('feed' in item)
                 await RSSFeedItem.flipSaved(item.id);
             else
                 await ReadingListItemDB.flipSaved(item.id);
 
             res.status(200).send();
-        } catch (e) {
-            res.status(500).send();
-        }
+        });
     });
 
     app.get('/feed-item', async (req, res) => {
-        try {
+        await WebUtils.defaultErrorHandling(req, res, async () => {
             const itemId = Number(req.query['itemID'])
-            if (!itemId || isNaN(itemId))
-                return res.status(400).send();
+            if (!itemId || isNaN(itemId)) {
+                res.status(400).send();
+                return;
+            }
 
             const feedItem = await RSSFeedItem.getById(Number(itemId));
             res.status(200).send(feedItem);
-        } catch (e) {
-            res.status(500).send();
-        }
+        });
     });
 
     app.get('/reading-list-item', async (req, res) => {
-        try {
+        await WebUtils.defaultErrorHandling(req, res, async () => {
             const itemId = Number(req.query['itemID'])
-            if (!itemId || isNaN(itemId))
-                return res.status(400).send();
+            if (!itemId || isNaN(itemId)) {
+                res.status(400).send();
+                return;
+            }
 
             const feedItem = await ReadingListItemDB.getById(Number(itemId));
             res.status(200).send(feedItem);
-        } catch (e) {
-            res.status(500).send();
-        }
-    })
+        });
+    });
 
     app.post('/reading-list-item', async (req, res) => {
-        let {url, title} = (req.body as {url: string, title?: string})
-        const domain = ReadingListItemDB.getDomain(url);
+        await WebUtils.defaultErrorHandling(req, res, async () => {
+            let {url, title} = (req.body as { url: string, title?: string })
+            const domain = ReadingListItemDB.getDomain(url);
 
-        if (!title)
-            title = await ReadingListItemDB.getArticleTitle(url);
+            if (!title)
+                title = await ReadingListItemDB.getArticleTitle(url);
 
-        const rlItem = {
-            link: url,
-            title,
-            pubDate: new Date().toISOString(),
-            domain
-        } as ReadingListItem;
+            const rlItem = {
+                link: url,
+                title,
+                pubDate: new Date().toISOString(),
+                domain
+            } as ReadingListItem;
 
-        try {
-            await ReadingListItemDB.insert(rlItem);
-        } catch (e) {
-            return res.status(400).send();
-        }
+            try {
+                await ReadingListItemDB.insert(rlItem);
+            } catch (e) {
+                Logger.debug(e);
+                res.status(400).send();
+                return;
+            }
 
-        res.status(200).redirect('/reading-list');
+            res.status(200).redirect('/reading-list');
+        });
     });
 }
